64e63a81c8b3ccff73e77f108b382ffedd719c60
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2024-07-12 14:21:15 +0530

    Move the `token` column from the APIKEY table to the SERVICE table
    Remove the APIKEY table from the entire database schema
    
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- webhook_to_fedora_messaging/models/service.py
+++ webhook_to_fedora_messaging/models/service.py
@@ -7,12 +7,15 @@ from webhook_to_fedora_messaging.models.util import UUIDCreatableMixin, Creatabl
 
 from webhook_to_fedora_messaging.database import Base
 
+from uuid import uuid4
+
 
 class Service(Base, UUIDCreatableMixin, CreatableMixin):
     __tablename__ = "services"
 
     id = Column(Integer, primary_key=True, nullable=False)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), unique=False, nullable=False)
+    token = Column(UnicodeText, unique=False, nullable=False, default=uuid4().hex)
     name = Column(UnicodeText, nullable=False)
     type = Column(UnicodeText, nullable=False)
     desc = Column(UnicodeText, nullable=False)
