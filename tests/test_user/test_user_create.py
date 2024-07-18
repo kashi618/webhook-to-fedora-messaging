70dc66b904fe8a98d70a3a340aa3f587a11d5fdf
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2024-07-18 13:47:29 +0530

    Add tests for the user creation endpoint
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

---
+++ tests/test_user/test_user_create.py
@@ -0,0 +1,31 @@
+import pytest
+
+
+@pytest.mark.parametrize(
+    "data, code",
+    [
+        pytest.param(
+            {"username": "mehmet"},
+            201,
+            id="USER Endpoint - 201 Created",
+        ),
+        pytest.param(
+            {"username": "mehmet"},
+            409,
+            id="USER Endpoint - 409 Conflict",
+        ),
+        pytest.param(
+            {"password": ""},
+            400,
+            id="USER Endpoint - 400 Bad Request"
+        ),
+        pytest.param(
+            None,
+            415,
+            id="USER Endpoint - 415 Unsupported Media Type"
+        )
+    ]
+)
+def test_user_create(client, data, code):
+    response = client.post("/user/", json=data)
+    assert response.status_code == code
