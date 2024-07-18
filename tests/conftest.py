7dc1ba9e07156c48ae73e22fd0e86357e1ca3038
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2024-07-18 13:46:55 +0530

    Add basic client configuration for testing
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,17 @@
 import pytest  # noqa: F401
 
 from webhook_to_fedora_messaging.database import db  # noqa: F401
+
+from webhook_to_fedora_messaging.main import create_app
+from os import environ
+from os.path import abspath, basename
+
+
+@pytest.fixture(scope="session")
+def client():
+    root = abspath(__name__)
+    environ["W2FM_APPCONFIG"] = root.replace(basename(root), "tests/data/test.toml")
+    with open("/tmp/w2fm-test.db", "wb") as dest:
+        with open(root.replace(basename(root), "tests/data/w2fm-test.db"), "rb") as srce:
+            dest.write(srce.read())
+    return create_app().test_client()
