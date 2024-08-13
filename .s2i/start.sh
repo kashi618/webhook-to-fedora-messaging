5810187531c3307ab38918dbb6cf1e23eceb94d3
Aurélien Bompard <aurelien@bompard.org>
2024-08-13 11:02:18 +0200

    Set some sane defaults for the s2i container startup
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ .s2i/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: Contributors to the Fedora Project
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+exec uvicorn \
+	--host 0.0.0.0 --port 8080 \
+	--log-config /etc/webhook-to-fedora-messaging/logging.yaml \
+	--factory webhook_to_fedora_messaging.main:create_app
