82991351b5ce6accb890cd302f8d0bf67b45f048
Aurélien Bompard <aurelien@bompard.org>
2024-08-14 09:24:10 +0200

    Configure logging in the CLI as well
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- webhook_to_fedora_messaging/cli.py
+++ webhook_to_fedora_messaging/cli.py
@@ -1,12 +1,14 @@
 import logging
+import logging.config
 import os
 from asyncio import run
 
 import click
+import yaml
 from sqlalchemy_helpers.aio import SyncResult
 
 from webhook_to_fedora_messaging import __version__
-from webhook_to_fedora_messaging.config import set_config_file
+from webhook_to_fedora_messaging.config import get_config, set_config_file
 from webhook_to_fedora_messaging.database import get_db_manager, setup_database
 
 
@@ -27,6 +29,9 @@ def main(conf=None):
     if conf:
         set_config_file(os.path.abspath(conf))
         get_db_manager.cache_clear()
+    config = get_config()
+    with open(config.logging_config) as fh:
+        logging.config.dictConfig(yaml.safe_load(fh))
 
 
 @main.command(name="setup", help="Setup the database schema in the specified environment")
